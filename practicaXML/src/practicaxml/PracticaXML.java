adc857eb474496e094c5f4c0891a6c16ef95fb97
hector <hector@Perenxisa-SA-00>
2016-12-07 10:13:46 +0100

    Hecho

--- practicaXML/src/practicaxml/PracticaXML.java
+++ practicaXML/src/practicaxml/PracticaXML.java
@@ -28,21 +28,15 @@ public class PracticaXML {
             DocumentBuilder builder = factoria.newDocumentBuilder();
             Document documento = builder.parse(new File ("jugadores.xml")) ;
             
-            NodeList jugadores=documento.getElementsByTagName("usuario");
-         // NodeList jugadores=documento.getDocumentElement().getChildNodes();
-         //   NodeList jugadores=documento.getChildNodes();
-           // jugadores=jugadores.item(0).getChildNodes();
-         
-            for (int i=0;i<jugadores.getLength();i++){
+            NodeList usuarios=documento.getElementsByTagName("usuario");
+       
+            System.out.println(usuarios.getLength());
+            for (int i=0;i<usuarios.getLength();i++){
                 System.out.println("---------------------------");
-                System.out.println(jugadores.item(i).getNodeName());
-                getDatosJugador(jugadores.item(i));
-            //    System.out.println(jugadores.item(i).getNodeValue());
+                getDatosJugador(usuarios.item(i));
                 
              
             }
-         //   leerNodo(documento.getFirstChild()); // recupera el primer nodo
-           // leerNodo(documento.getDocumentElement()); // recupera el elemento raíz
             
         } catch (ParserConfigurationException | SAXException | IOException ex) {
             Logger.getLogger(PracticaXML.class.getName()).log(Level.SEVERE, null, ex);
@@ -51,20 +45,13 @@ public class PracticaXML {
     }
     public static void getDatosJugador(Node nodo) {
         
-        NodeList datos=nodo.getChildNodes();
-        System.out.println("Nombre: " + datos.item(0).getNodeValue());
-        System.out.println("Horas: " + datos.item(1).getNodeValue());
-        System.out.println("Puntuación: " + datos.item(2).getNodeValue());
-        System.out.println("Nivel: " + datos.item(3).getNodeValue());
-        System.out.println("");
+        Element elem = (Element)nodo; 
+        NodeList datos=elem.getChildNodes();
+        for (int j=0;j<datos.getLength();j++){
+            if (datos.item(j).getNodeType()==1){
+                System.out.println(datos.item(j).getNodeName()+":"+datos.item(j).getTextContent());
+            }
         
-       /* System.out.println("-------");
-        NodeList listaHijos = nodo.getChildNodes();
-        int num = listaHijos.getLength();
-        for (int i = 0; i < num ; i++) {
-            Node nodoActual = listaHijos.item(i);
-            leerNodo(nodoActual);
-        }*/
+       }
     }
-    
 }
